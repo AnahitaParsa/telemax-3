4c89fbbc077d60180c0eb29d94bdd0b6a1b74405
AnahitaParsa <ana0parsa6@gmail.com>
2016-03-31 16:48:02 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -227,7 +227,7 @@ function create_config( )
     "plugins",
     "admin"
     },
-    sudo_users = {197092693,115887468,0,tonumber},--Sudo users
+    sudo_users = {197092693,115887468},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
